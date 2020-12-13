f87856d4a6be8c3a4ba9b5e7ed44a84f2f2a1ad7
Geovani José <geovani-lol@hotmail.com>
2020-12-13 01:03:52 -0300

    Implement Historic page

--- src/pages/Historic/index.tsx
+++ src/pages/Historic/index.tsx
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+
+import { useHistoric } from '../../hooks/historic';
+import formatValue from '../../utils/formatValue';
 
 import Header from '../../components/Header';
 
-import { Container } from './styles';
+import { Container, Content, Table } from './styles';
 
 const Historic: React.FC = () => {
+  const { historic } = useHistoric();
+
+  const formattedPrices = useCallback(
+    price => (price >= 0 ? formatValue(price) : '-'),
+    [],
+  );
+
   return (
     <Container>
       <Header />
 
-      <h1>Historic</h1>
+      <Content>
+        <h1>Histórico de cálculos</h1>
+
+        <Table>
+          <thead>
+            <tr>
+              <th>Origem</th>
+              <th>Destino</th>
+              <th>Tempo</th>
+              <th>Plano</th>
+              <th>Sem FaleMais</th>
+              <th>Com FaleMais</th>
+            </tr>
+          </thead>
+          <tbody>
+            {historic
+              .slice(0)
+              .reverse()
+              .map(hist => (
+                <tr key={hist.id}>
+                  <td>{hist.origem}</td>
+                  <td>{hist.destino}</td>
+                  <td>{hist.tempo}</td>
+                  <td>{hist.plano}</td>
+                  <td>{formattedPrices(hist.price)}</td>
+                  <td>{formattedPrices(hist.priceDiscount)}</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Content>
     </Container>
   );
 };
--- src/pages/Historic/styles.ts
+++ src/pages/Historic/styles.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Content = styled.main`
+  flex: 1;
+  width: 100%;
+  max-width: 820px;
+  margin: 150px auto 0;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    margin-bottom: 28px;
+  }
+`;
+
+export const Table = styled.table`
+  border: 6px solid #f4f2ed;
+  border-collapse: collapse;
+
+  th {
+    padding: 12px 0;
+    color: #f4f2ed;
+    border: 4px solid #f4f2ed;
+    background-color: #4a3a23;
+  }
+
+  td {
+    padding: 8px;
+    text-align: center;
+    color: #ae8952;
+    border: 0 solid #f4f2ed;
+    border-width: 8px 0;
+    background-color: #fff;
+  }
+`;
